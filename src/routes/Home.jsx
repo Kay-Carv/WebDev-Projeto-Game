0eb587a89f6444b1f235f17221f8343937909f6a
Kayque <kayquecarvalho020@gmail.com>
2025-04-07 12:04:43 -0300

    Adcionando dados e produtos na home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Produtos from "./Produtos"
 
-const Home = () => {
+// Dados vai chamar os nossos dados
+// map percorre todos os itens para trazer para a gente
+const Home = ({Dados}) => {
   return (
     <div>
-      
+      {Dados.map(produto =>{
+        <Produtos
+        key={produto.id}
+        imagem={produto.imagem}
+        titulo={produto.titulo}
+        descricao={produto.descricao}
+        preco={produto.preco}
+        />
+      })}
     </div>
   )
 }
