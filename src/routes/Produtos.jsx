989d71880ddc92da5cdbc94c02e6ff345db04d6f
Kayque <kayquecarvalho020@gmail.com>
2025-04-07 12:05:41 -0300

    Preparando componente de produtos e criando base para estilização

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import { ProdutoContainer } from "../styles/ProdutoStyled"
 
 // Importando imagens via props
 // props - Vai pegar dados de outro local 
 const Produtos = (imagem, titulo, descricao, preco) => {
   return (
-    <article className="produto">
-      <img src={imagem} alt={titulo} />
-      <h3>{titulo}</h3>
-      <p>{descricao}</p>
-      <p className="preco">{preco}</p>
-      <button>Comprar</button>
-
-    </article>
+    <ProdutoContainer>
+      <article className="produto">  
+        <img src={imagem} alt={titulo}/>
+        <h3>{titulo}</h3>
+        <p>{descricao}</p>
+        <p className="preco">{preco}</p>
+        <button>Comprar</button>
+      </article>
+    </ProdutoContainer>
   )
 }
 
