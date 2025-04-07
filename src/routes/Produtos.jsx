9b1a29e8862b30680878b79a8853e5d4a4005dcc
Kayque <kayquecarvalho020@gmail.com>
2025-04-07 11:23:27 -0300

    Preparando o componente produto

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const Produtos = () => {
+// Importando imagens via props
+// props - Vai pegar dados de outro local 
+const Produtos = (imagem, titulo, descricao, preco) => {
   return (
-    <div>
-      
-    </div>
+    <article className="produto">
+      <img src={imagem} alt={titulo} />
+      <h3>{titulo}</h3>
+      <p>{descricao}</p>
+      <p className="preco">{preco}</p>
+      <button>Comprar</button>
+
+    </article>
   )
 }
 
