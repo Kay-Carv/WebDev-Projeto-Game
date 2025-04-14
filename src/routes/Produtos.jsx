13cb90d396db4521c715662b88a507fc409ca1e8
Kayque <kayquecarvalho020@gmail.com>
2025-04-14 11:58:27 -0300

    Ajustando o consumo da API dos produtos e ajustando styles dos produtos

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -2,7 +2,7 @@ import { ProdutoContainer } from "../styles/ProdutoStyled"
 
 // Importando imagens via props
 // props - Vai pegar dados de outro local 
-const Produtos = (imagem, titulo, descricao, preco) => {
+const Produtos = ({imagem, titulo, descricao, preco}) => {
   return (
     <ProdutoContainer>
       <article className="produto">  
@@ -10,7 +10,7 @@ const Produtos = (imagem, titulo, descricao, preco) => {
         <h3>{titulo}</h3>
         <p>{descricao}</p>
         <p className="preco">{preco}</p>
-        <button>Comprar</button>
+        <button className="btn">Comprar</button>
       </article>
     </ProdutoContainer>
   )
--- src/styles/ProdutoStyled.jsx
+++ src/styles/ProdutoStyled.jsx
@@ -9,4 +9,33 @@ border:1px solid #ddd;
 background-image: linear-gradient(#2b86c5,#784ba0,#ff3CAC);
 border-radius:30px;
 color:#fff;
+
+img {
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 10px;
+}
+
+.preco {
+    font-weight: bold;
+    margin-top: 50px;
+    padding-left:50px;
+    color: greenyellow;
+    font-size: 1.8rem;
+}
+
+.btn {
+    background-color:#05192e;
+    margin: 20px 30px;
+    color: yellowgreen;
+    font-weight:800;
+    padding: 20px 40px;
+    border-radius: 20px;
+    cursor: pointer;
+    text-transform:uppercase;
+    &:hover {
+    background: greenyellow;
+    color: #000;
+}
+
 `
